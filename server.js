5202aa94f0fed7cefc7a408c4caa71cae67b9b7a
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-18 17:57:02 +0100

    invoking express into js file

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 const fs = require ('fs')
+const express = express ('express')
+const path = require('path')
+// invoking express
+const app = express()
+const PORT = 3001;
+
+app.use(express.static('public'))
+
