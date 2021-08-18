80190d1352f33b211cb3a774868e9a561dcc84d1
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-18 18:19:18 +0100

    route to serve the notes.html & index.html

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const PORT = 3001;
 
 app.use(express.static('public'))
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/notes', (req,res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
