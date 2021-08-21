9d40322b7224772e7bbd1b95e92f02285682d7f3
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-21 23:57:47 +0100

    chaanged functions from arrow syntax to basic

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public/index.html'))});
+app.get('/',function  (req, res) { res.sendFile(path.join(__dirname, 'public/index.html'))});
 
-app.get('/notes', (req,res) =>{ res.sendFile(path.join(__dirname, 'public/notes.html'))})
+app.get('/notes', function (req,res) { res.sendFile(path.join(__dirname, 'public/notes.html'))})
 // route for json file 
 pp.get("/api/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "db/db.json"));})
