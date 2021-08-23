9c2c521c17f116c5421d13b6761b2a2f977883b4
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-23 02:35:41 +0100

    removed uuid call back

--- Routes/api.js
+++ Routes/api.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 
 const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+ // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
