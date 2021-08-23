a2ba1c0f572645044b63625419f5a488c624183c
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-23 01:21:37 +0100

    changed routes from server js to separate

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-const express = require ('express')
-const fs = require ('fs')
+const express = require("express")
 const path = require('path')
 // invoking express
 const app = express()
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
-
-app.get('/',function  (req, res) { res.sendFile(path.join(__dirname, 'public/index.html'))});
-
-app.get('/notes', function (req,res) { res.sendFile(path.join(__dirname, 'public/notes.html'))})
-// route for json file 
-pp.get("/api/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "db/db.json"));})
-
+app.use(express.static("public"))
 
  app.listen(PORT,function (){
-  console.log(`Example app listening at http://localhost:${PORT}`)});
+  console.log(`Example app listening at https://localhost:${PORT}`)});
   
