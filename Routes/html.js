2d3272f7cde865afb2f4c0cf89a801d6542614ef
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-23 01:27:28 +0100

    moved all get requests into a function

--- Routes/html.js
+++ Routes/html.js
@@ -1,5 +1,6 @@
 const path = require("path")
 
+module.exports = function(app){
 // included app function to contain all get requests
 app.get('/notes', function (req,res) { res.sendFile(path.join(__dirname, '/public/notes.html'))})
-app.get('*',function  (req, res) { res.sendFile(path.join(__dirname, '/public/index.html'))});
+app.get('*',function  (req, res) { res.sendFile(path.join(__dirname, '/public/index.html'))})}
