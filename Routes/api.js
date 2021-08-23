15df4525476f8c6693f8a9bb8ecd0a1c2d430822
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-23 02:02:05 +0100

    added random UUID

--- Routes/api.js
+++ Routes/api.js
@@ -1,4 +1,10 @@
+const fs = require("fs");
+
+
+const { v4: uuidv4 } = require('uuid');
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
 // route for json file 
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function(req, res) u9ij{
   res.sendFile(path.join(__dirname, "db/db.json"));})
+ 
