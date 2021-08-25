08ec168219b94a690b0e98c0d259825310b84ce7
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-08-25 18:31:19 +0100

    Removed route files & corrected request & response

--- Routes/html.js
+++
@@ -1,6 +0,0 @@
-const path = require("path")
-
-module.exports = function(app){
-// included app function to contain all get requests
-app.get('/notes', function (req,res) { res.sendFile(path.join(__dirname, '/public/notes.html'))})
-app.get('*',function  (req, res) { res.sendFile(path.join(__dirname, '/public/index.html'))})}
--- server.js
+++ server.js
@@ -1,14 +1,45 @@
-const express = require("express")
-const path = require('path')
+const express = require("express");
+const path = require("path");
 // invoking express
-const app = express()
+const { v4: uuidv4 } = require('uuid');
+const fs = require("fs")
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
- app.listen(PORT,function (){
-  console.log(`Example app listening at https://localhost:${PORT}`)});
-  
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  console.log("New Note generated:" + JSON.stringify(newNote));
+
+  newNote.id = uuidv4();
+
+  let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+  noteData.push(newNote);
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteData));
+
+  res.json(newNote);
+});
+app.get("/api/notes", function (req, res) {
+  console.log("Get note requests intiated");
+
+  let noteData = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  console.log("note data returned:" + JSON.stringify(noteData));
+  res.json(noteData);
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`Example app listening at https://localhost:${PORT}`);
+});
